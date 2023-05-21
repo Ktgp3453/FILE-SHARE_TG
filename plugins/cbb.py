03d86e86e045eda9773a83ce75e0b2e7d782ca1a
Ktgp3453 <kaivalyadoiphode987@gmail.com>
2023-05-21 11:41:43 -0600

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @CodeXBotz\n○ Support Group : @CodeXBotzSupport</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @Otaku_Library\n○ Chat Group : @Otaku_Chats</b>\n○ Admin 1 : @Psycho_Gecko\n○ Admin 2 : @Call_Me_Blank\n○ Admin 3 : @Otaku_Helper",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
