11158a3d454c7d45e2e23e694629fdafa1475916
Ktgp3453 <kaivalyadoiphode987@gmail.com>
2023-06-30 15:31:49 +0530

    Update start.py
    button edited

--- plugins/start.py
+++ plugins/start.py
@@ -88,8 +88,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("🧐 About Bot", callback_data = "about"),
+                    InlineKeyboardButton("😶‍🌫️ Close", callback_data = "close")
                 ]
             ]
         )
